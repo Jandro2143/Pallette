df7455cf09615cc7a56c46d161d814a034a284f9
Alejandro Cedillo <alex.cedillo.e@gmail.com>
2024-02-14 15:27:19 +1100

    Set up

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+// Import PrimeVue styles
+import 'primevue/resources/themes/saga-blue/theme.css'; // theme
+import 'primevue/resources/primevue.min.css'; // core css
+import 'primeicons/primeicons.css'; // icons
+
+const app = createApp(App);
+
+// Use PrimeVue
+app.use(PrimeVue);
+
+// Optionally, you can add PrimeVue components globally
+import Button from 'primevue/button';
+app.component('Button', Button);
+
+app.mount('#app');
